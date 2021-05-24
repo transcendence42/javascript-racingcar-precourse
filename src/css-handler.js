b984ec91e94b60ee3cf8ba8ac00604a6734308b9
hochan Lee <hochan049@gmail.com>
2021-05-24 15:16:08 +0900

    [REFACTOR] == to ===

--- src/css-handler.js
+++ src/css-handler.js
@@ -1,6 +1,6 @@
 export default function toggleTagDisplay(id) {
   const $tagStyleDisplay = document.getElementById(id);
-  if ($tagStyleDisplay.style.display == 'none') {
+  if ($tagStyleDisplay.style.display === 'none') {
     $tagStyleDisplay.style.display = 'block';
   } else {
     $tagStyleDisplay.style.display = 'none';
