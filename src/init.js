5c6c96f23529114b841afc2974f8992b8ded8604
hochan Lee <hochan049@gmail.com>
2021-05-24 10:48:00 +0900

    [FEAT] add checkValidCarName

--- src/init.js
+++ src/init.js
@@ -1,40 +1,54 @@
 'use strict';
 
 /*
- ** Input
- */
-
-function getInputCarName() {
-  let $carNamesInput = document.getElementById('car-names-input');
-  console.log($carNamesInput);
-}
-
-/*
- ** Event Listener
- */
-
-function addButtonEvent(buttonId, func) {
-  const $submitButton = document.getElementById(buttonId);
-  $submitButton.addEventListener('click', () => func());
-}
-
-function clickCarNamesSubmit() {
-  getInputCarName();
-}
-
-function clickRacingCountSubmit() {
-  console.log('bye');
-}
-
-function registerButtonEvent() {
-  addButtonEvent('car-names-submit', clickCarNamesSubmit);
-  addButtonEvent('racing-count-submit', clickRacingCountSubmit);
-}
-
-/*
  ** Init
  */
 
 export default function initGame() {
   registerButtonEvent();
+
+  return;
+  /*
+   ** Check
+   */
+
+  function checkValidCarName(carNames) {
+    const validCarName = carNames.filter(
+      (carName) => carName.length <= 5 && carName != '',
+    );
+    return validCarName.length === carNames.length;
+  }
+
+  /*
+   ** Input
+   */
+
+  function getInputCarName() {
+    return document.getElementById('car-names-input').value.split(',');
+  }
+
+  /*
+   ** Event Listener
+   */
+
+  function addButtonEvent(buttonId, func) {
+    const $submitButton = document.getElementById(buttonId);
+    $submitButton.addEventListener('click', () => func());
+  }
+
+  function clickCarNamesSubmit() {
+    if (!checkValidCarName(getInputCarName())) {
+      alert('hi');
+    }
+    console.log('2');
+  }
+
+  function clickRacingCountSubmit() {
+    console.log('bye');
+  }
+
+  function registerButtonEvent() {
+    addButtonEvent('car-names-submit', clickCarNamesSubmit);
+    addButtonEvent('racing-count-submit', clickRacingCountSubmit);
+  }
 }
